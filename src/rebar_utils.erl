7093aa83fdae984b485bfdf631570929d5c380e8
Tuncer Ayaz <tuncer.ayaz@gmail.com>
2012-01-03 18:57:34 +0100

    Simplify and extend rebar_utils:deprecated

--- src/rebar_utils.erl
+++ src/rebar_utils.erl
@@ -42,7 +42,7 @@
          find_executable/1,
          prop_check/3,
          expand_code_path/0,
-         deprecated/4, deprecated/5,
+         deprecated/3, deprecated/4,
          expand_env_variable/3,
          vcs_vsn/2,
          get_deprecated_global/3]).
@@ -233,6 +233,29 @@ vcs_vsn(Vcs, Dir) ->
             end
     end.
 
+deprecated(Old, New, Opts, When) when is_list(Opts) ->
+    case lists:member(Old, Opts) of
+        true ->
+            deprecated(Old, New, When);
+        false ->
+            ok
+    end;
+deprecated(Old, New, Config, When) ->
+    case rebar_config:get(Config, Old, undefined) of
+        undefined ->
+            ok;
+        _ ->
+            deprecated(Old, New, When)
+    end.
+
+deprecated(Old, New, When) ->
+    io:format(
+      <<"WARNING: deprecated ~p option used~n"
+        "Option '~p' has been deprecated~n"
+        "in favor of '~p'.~n"
+        "'~p' will be removed ~s.~n~n">>,
+      [Old, Old, New, Old, When]).
+
 get_deprecated_global(OldOpt, NewOpt, When) ->
     case rebar_config:get_global(NewOpt, undefined) of
         undefined ->
@@ -331,22 +354,6 @@ emulate_escript_foldl(Fun, Acc, File) ->
         {error, _} = Error ->
             Error
     end.
-
-deprecated(Key, Old, New, Opts, When) ->
-    case lists:member(Old, Opts) of
-        true ->
-            deprecated(Key, Old, New, When);
-        false ->
-            ok
-    end.
-
-deprecated(Key, Old, New, When) ->
-    io:format(
-      <<"WARNING: deprecated ~p option used~n"
-        "Option '~p' has been deprecated~n"
-        "in favor of '~p'.~n"
-        "'~p' will be removed ~s.~n~n">>,
-      [Key, Old, New, Old, When]).
 
 vcs_vsn_cmd(git) ->
     %% Explicitly git-describe a committish to accommodate for projects
